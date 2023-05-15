6094b34661176a8c7f1edb09b19c3b9af712dba2
Jaymerson <jaymerson.ferreira.s@gmail.com>
2023-05-15 01:20:06 -0300

    Adicionado algumas edicoes nos textos

--- individual.py
+++ individual.py
@@ -7,22 +7,41 @@ def resultado(criterios, candidato):
         s = candidato[i][3]
 
         result.append(f'{i+1}ª Candidato: e{e}_t{t}_p{p}_s{s}')
+
+    print('-' * 30) 
+    print('\033[34mCandidatos\033[m')  
+    print('_' * 30) 
+
     for c in range(len(candidato)):
-        print(f'{result[c]}')
+        print(f'{result[c][:13]:.<15}', end='')
+        print(f'{result[c][13:]:.>15}')
+      
     
     for j in range(len(candidato)):
         cont = 0
         for i in range(0, 4):
             if 0 < criterios[i] <= candidato[j][i]:
                 cont += 1
-        if cont == 4:
-            lista_aprovados.append(f'{j+1}ª Candidato: e{e}_t{t}_p{p}_s{s} APROVADO')
-    return lista_aprovados
-    
+            if cont == 4:
+                lista_aprovados.append(f'{j+1}ª Candidato: e{e}_t{t}_p{p}_s{s} APROVADO')
+
+    print('-' * 30)  
+    print('\033[34mLista dos aprovados\033[m')
+    print('_' * 30)  
+
+    for ap in range(len(lista_aprovados)):
+        print(f'{lista_aprovados[ap][:13]:.<18}', end='')
+        print(f'{lista_aprovados[ap][13:25]}')
+
 
 lista_aprovados = []
 result = []
 candidato = []
+
+print('-' * 30)    
+print('\033[34mQuais são as notas:\033[m')
+print('_' * 30)    
+
 while True:
     notas = [int(input('Nota para Entrevista: ')),
             int(input('Nota para Teste Teorico: ')),
@@ -34,16 +53,18 @@ while True:
     resposta = input('Quer continuar? ').lower()[0]
     if resposta == 'n':
         break
+    print()
+
+print('-' * 30)    
+print('\033[34mQuais são Critério:\033[m')
+print('_' * 30)    
 
 criterios = [int(input('e_ Entrevista: ')),
              int(input('t_ Teste Teórico: ')),
              int(input('p_ Teste Prático: ')),
-             int(input('s_ Avaliação de Soft Skill: '))]
-
-print(candidato)
+             int(input('s_ Soft Skill: '))]
 
 resultado(criterios, candidato)
 
-print(lista_aprovados)
 
 
