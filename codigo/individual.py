acfbf15cb8de2e87992a124896cbbc76cb872fc2
Jaymerson <jaymerson.ferreira.s@gmail.com>
2023-05-18 23:44:30 -0300

    Adicionado comentáriose troca o condidato por ficha

--- codigo/individual.py
+++ codigo/individual.py
@@ -4,13 +4,14 @@ def titulo(msg):
         print(f'\033[34m{msg}\033[m')  
         print('-' * 30)
 
-def lista_candidato(candidato):
+#Imprime os candidato
+def lista_candidato(ficha):
     global result, lista_aprovados
-    for i in range(len(candidato)):
-        e = candidato[i][0]
-        t = candidato[i][1]
-        p = candidato[i][2]
-        s = candidato[i][3]
+    for i in range(len(ficha)):
+        e = ficha[i][0]
+        t = ficha[i][1]
+        p = ficha[i][2]
+        s = ficha[i][3]
 
         result.append(f'{i+1}ª Candidato: e{e}_t{t}_p{p}_s{s}')
         
@@ -20,15 +21,15 @@ def lista_candidato(candidato):
         print(f'{result[c][:13]:.<18}', end='')
         print(f'{result[c][13:]}')
     
-
-def aprovados(criterios, candidato):
-    for j in range(len(candidato)):
+#Imprime os aprovados
+def aprovados(criterios, ficha):
+    for j in range(len(ficha)):
         cont = 0
         for i in range(0, 4):
-            if 0 < criterios[i] <= candidato[j][i]:
+            if 0 < criterios[i] <= ficha[j][i]:
                 cont += 1
             if cont == 4:
-                lista_aprovados.append(f'{j+1}ª Candidato: e{candidato[j][0]}_t{candidato[j][1]}_p{candidato[j][2]}_s{candidato[j][3]}')
+                lista_aprovados.append(f'{j+1}ª Candidato: e{ficha[j][0]}_t{ficha[j][1]}_p{ficha[j][2]}_s{ficha[j][3]}')
 
     titulo('Lista dos aprovados:')
      
@@ -38,21 +39,21 @@ def aprovados(criterios, candidato):
     if len(lista_aprovados) == 0:
         print('\033[31mNão há aprovados!\033[m')
 
-
+#variáveis
 lista_aprovados = []
-candidato = []
+ficha = []
 result = []
 pos = 1
   
 titulo('Quais são as notas:')
-   
+#Recebe as notas   
 while True:
     print(f'{pos}ª Candidato:')
     notas = [int(input('Nota para Entrevista: ')),
             int(input('Nota para Teste Teorico: ')),
             int(input('Nota para Teste Prático: ')),
             int(input('Nota para Soft Skill: '))]
-    candidato.append(notas[:])
+    ficha.append(notas[:])
     notas.clear
 
     resposta = input('Quer continuar? [N] para sair: ').lower()[0]
@@ -62,14 +63,12 @@ while True:
     pos +=1
    
 titulo('Quais são os Critérios:')
-   
+#Rebece os critérios   
 criterios = [int(input('e_ Entrevista: ')),
              int(input('t_ Teste Teórico: ')),
              int(input('p_ Teste Prático: ')),
              int(input('s_ Soft Skill: '))]
 
-lista_candidato(candidato)
+lista_candidato(ficha)
 
-aprovados(criterios, candidato)
-
-
+aprovados(criterios, ficha)
